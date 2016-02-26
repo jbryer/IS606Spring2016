4a3e686e038c59a9c59eee7166e9b9afb6ad1347
Jason Bryer <jason@bryer.org>
2016-02-25 19:52:36 -0500

    Uploaded slides.

---
+++ R/ShinyApps.R
@@ -0,0 +1,10 @@
+library(IS606)
+library(psa)
+
+shiny_demo('lottery')
+runApp('~/Dropbox/Projects/DAACS-Github/DAACS/shiny/AssessmentTest')
+psa_shiny()
+
+# Other apps
+shiny_demo('BayesBilliards')
+shiny_demo('gambler')
